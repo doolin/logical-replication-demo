cadc8f35490b78b1d030c79e27bcc4b38cb6cae2
Dave Doolin <david.doolin@gmail.com>
2023-11-03 05:47:24 -0700

    GEN-195 add influxdb via docker container (#2)
    One of the goals of this project is examining postgres
    internal, for which influx will provide a backing store
    for query results. This commit adds a docker file and
    script support for push button provisioning.

--- restart.sh
+++ restart.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME="pubsub"
 DOCKERFILE_PATH="."
 CONFIG_FILE_PATH="."
-CONTAINERS=("subscriber1" "subscriber2" "publisher")
+CONTAINERS=("subscriber1" "subscriber2" "publisher" "pubmetrics")
 
 for CONTAINER_NAME in "${CONTAINERS[@]}"; do
   if docker ps -a | grep -qw $CONTAINER_NAME; then
@@ -12,10 +12,16 @@ for CONTAINER_NAME in "${CONTAINERS[@]}"; do
   fi
 done
 
+# Locally defined Docker file.
 docker buildx build . -t $IMAGE_NAME # -f $DOCKERFILE_PATH .
-docker run --name subscriber1 -p 5433:5432 -e POSTGRES_PASSWORD=foobar -d pubsub
-docker run --name subscriber2 -p 5434:5432 -e POSTGRES_PASSWORD=foobar -d pubsub
-docker run --name publisher -p 5435:5432 -e POSTGRES_PASSWORD=foobar -d pubsub
+docker run -d --name subscriber1 -p 5433:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
+docker run -d --name subscriber2 -p 5434:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
+docker run -d --name publisher -p 5435:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
+
+# Pull from Docker Hub.
+docker buildx build -t pubmetrics -f Dockerfile.influxdb .
+docker run -d --name pubmetrics -p 8086:8086 -v myInfluxVolume:/var/lib/influxdb2 pubmetrics
+
 
 # Optional: Remove old Docker images to free up space
 # docker system prune -a
