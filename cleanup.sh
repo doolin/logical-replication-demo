cf7c397adaee5b1220b268c455edaf26ec289854
Dave Doolin <david.doolin@gmail.com>
2023-11-19 08:38:08 -0800

    GEN-213 Parameterize pgbench script (#19)
    https://doolin.atlassian.net/browse/GEN-213
    
    Implemented command line options using `getopt` for the
    pgbench wrapper script. These will likely be superfluous
    if I implement a Ruby driver script for easier scenario
    parameterization and randomization, but still useful in
    the near term.
    
    Shellcheck was run on both pgbench.sh and cleanup.sh with
    some minor fixes to get clean passes. The grafana image
    was added to cleanup.sh, necessary to get a complete test
    cycle to work.

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# shellcheck disable=SC1091
+# TODO: should consider running shellcheck -x
 source ./ansi_colors.sh
 source ./helpers.sh
 infotext "This will stop and remove the subscriber docker containers"
 press_enter
 
-containers=("subscriber1" "subscriber2" "publisher" "pubmetrics")
+containers=("subscriber1" "subscriber2" "publisher" "pubmetrics" "grafana")
 for container in "${containers[@]}"; do
   docker stop "$container" >/dev/null 2>&1 # stdout and stderr to /dev/null
   docker rm "$container" >/dev/null 2>&1
@@ -14,11 +16,11 @@ done
 # The nuclear option, deletes all containers and images
 # which aren't running.
 infotext "Do you want to nuke ALL docker containers and images?"
-read nukem
+read -r nukem
 if [[ $nukem == "yes" ]]; then
   docker system prune -af && \
       docker image prune -af && \
-      docker system prune -af --volumes && \ # deletes build cache objects
+      docker system prune -af --volumes &&
       docker system df # shows disk usage
 else
   infotext "Not nuking docker containers and images."
