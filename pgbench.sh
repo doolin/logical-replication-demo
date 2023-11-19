05d16fe1debd3e0d753709a85e8b2ff5382f691c
dave doolin <david.doolin@gmail.com>
2023-11-19 07:43:49 -0800

    GEN-213 Parameterize pgbench script
    https://doolin.atlassian.net/browse/GEN-213
    
    Implemented command line options using `getopt` for the
    pgbench wrapper script. These will likely be superfluous
    if I implement a Ruby driver script for easier scenario
    parameterization and randomization, but still useful in
    the near term.
    
    Shellcheck was run on both pgbench.sh and cleanup.sh with
    some minor fixes to get clean passes. The grafana image
    was added to cleanup.sh, necessary to get a complete test
    cycle to work.

--- pgbench.sh
+++ pgbench.sh
@@ -1,6 +1,64 @@
 #!/bin/bash
 
-# TODO: parameterize the pg bench arguments
+# DEFAULT_MEMORY="512m"
+# MEMORY=$DEFAULT_MEMORY
+DURATION=60
+SCALE=10
+CLIENTS=10
+THREADS=3
+
+# Function to show help
+show_help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -T <seconds>   Duration of the benchmark in seconds"
+    echo "  -c <clients>   Number of clients to simulate"
+    echo "  -j <threads>   Number of threads (jobs) to use, must be less than or equal to the number of clients"
+    echo "  -h             Display this help and exit"
+}
+
+# Process command-line options
+while getopts ":hT:c:j:" opt; do
+    case ${opt} in
+        h )
+            show_help
+            exit 0
+            ;;
+        T )
+            DURATION="${OPTARG}"
+            # Check if the provided duration is a number
+            if ! [[ "$DURATION" =~ ^[0-9]+$ ]]; then
+                echo "Invalid duration: $DURATION. Duration must be a number."
+                exit 1
+            fi
+            ;;
+        c )
+            CLIENTS="${OPTARG}"
+            # Check if the provided number of clients is a number
+            if ! [[ "$CLIENTS" =~ ^[0-9]+$ ]]; then
+                echo "Invalid number of clients: $CLIENTS. Number of clients must be a number."
+                exit 1
+            fi
+            ;;
+        j )
+            THREADS="${OPTARG}"
+            # Check if the provided number of threads is a number
+            if ! [[ "$THREADS" =~ ^[0-9]+$ ]]; then
+                echo "Invalid number of threads: $THREADS. Number of threads must be a number."
+                exit 1
+            fi
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            show_help
+            exit 1
+            ;;
+        : )
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
 
 DB_NAME="publisher"
 PG_USER="postgres"
@@ -18,19 +76,18 @@ else
     # Initialize the database
     # -s is scale factor
     # -i is initialize
-    PGPASSWORD=foobar pgbench -h $HOST -p $PORT -U $PG_USER -i -s 10 $DB_NAME
+    PGPASSWORD=foobar pgbench -h $HOST -p $PORT -U $PG_USER -i -s $SCALE $DB_NAME
 fi
 
 # Run the benchmark
 # -T is duration in seconds
 # -c is number of clients
-# -j is number of threads
-PGPASSWORD=foobar pgbench -h $HOST -p $PORT -U $PG_USER -T 600 -c 10 -j 2 $DB_NAME
+# -j is number of threads, needs to be less than or equal to the number of clients
+PGPASSWORD=foobar pgbench -h $HOST -p $PORT -U $PG_USER -T "$DURATION" -c "$CLIENTS" -j "$THREADS" $DB_NAME
 
 # This is a second benchmark to run in parallel
 # This should go away once the script is parameterized
 # PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -T 60 -c 2 -j 2 publisher
-
 
 # TODO: clean up the database
 # Function to drop a table
