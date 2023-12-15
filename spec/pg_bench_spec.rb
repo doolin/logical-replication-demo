829ca09e46ed468eedb18bf610d9616b93e4ddea
dave doolin <david.doolin@gmail.com>
2023-12-15 04:21:58 -0800

    GEN-261 Add remaining pgbench options to Ruby driver
    https://doolin.atlassian.net/browse/GEN-261
    
    The Ruby script for running pg bench now has all the
    pgbench options wired up. This will be useful for
    running experiments.

--- spec/pg_bench_spec.rb
+++ spec/pg_bench_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PGBench do
   describe '#default' do
     it 'returns the default pgbench command' do
       expect(described_class.new.default)
-        .to eq('PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -T 60 -c 10 -j 3 publisher')
+        .to eq('PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -s 10 -T 60 -c 10 -j 3 publisher')
     end
   end
 
@@ -18,7 +18,7 @@ RSpec.describe PGBench do
     end
 
     it 'calls bench_sys with default command' do
-      default_command = "PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -T 60 -c 10 -j 3 publisher"
+      default_command = "PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -s 10 -T 60 -c 10 -j 3 publisher"
       pgbench.run_default
 
       expect(pgbench).to have_received(:bench_sys).with(default_command)
