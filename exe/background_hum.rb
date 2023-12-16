424152843fdec600ad2c78e10fc7074bcc4c7faa
dave doolin <david.doolin@gmail.com>
2023-12-15 18:04:12 -0800

    GEN-262 Create a "background hum" pgbench driver
    https://doolin.atlassian.net/browse/GEN-262
    
    This file will run pgbench for 10 minutes to help
    soak up some memory and CPU from the container.
    Should make it easier to mess things up when
    applying randon loads.

---
+++ exe/background_hum.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../lib/pg_bench'
+
+# Run a "background" benchmark for 10 minutes.
+# This is to chew up mamory and CPU so we can see
+# how the sampler reacts when random loads are applied.
+options = {
+  scale: 10,   # Default scale
+  threads: 3,  # Default threads
+  clients: 10, # Default clients
+  duration: 600  # Default duration
+}
+
+
+puts "Configured options: #{options.inspect}"
+PGBench.new(options).run
