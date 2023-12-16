0e0187a5b68b403498b953b05bc389ddc9feb7ea
dave doolin <david.doolin@gmail.com>
2023-12-15 17:46:46 -0800

    GEN-263 Grafana dashboard via API
    https://doolin.atlassian.net/browse/GEN-263
    
    Have a dashboard ready to be used when Grafana
    is rebuilt. A collection of script (including perl)
    and json files kludge together a preliminary
    attempt at automating data display and monitoring.
    It's pretty ugly, but it works.

---
+++ scripts/grafana_dashboard.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+
+# Grafana API details
+GRAFANA_HOST="http://localhost:3000"
+GRAFANA_API_TOKEN=$GRAFANA_API_TOKEN
+
+# Dashboard JSON file
+DASHBOARD_JSON="grafana_dashboard.json"
+
+# Check if the dashboard JSON file exists
+if [ ! -f "$DASHBOARD_JSON" ]; then
+    echo "Dashboard JSON file not found: $DASHBOARD_JSON"
+    exit 1
+fi
+
+
+# API URL for dashboard creation
+API_URL="$GRAFANA_HOST/api/dashboards/db"
+
+# Make the API request to create the dashboard
+curl -X POST "$API_URL" \
+     -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
+     -H "Content-Type: application/json" \
+     -d @"$DASHBOARD_JSON"
+
+# Check if the dashboard was created successfully
+if [ $? -eq 0 ]; then
+    echo "Dashboard created successfully."
+else
+    echo "Failed to create dashboard."
+fi
