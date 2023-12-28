8709d0f968bfef4237577ed22c88e76407e2d189
dave doolin <david.doolin@gmail.com>
2023-12-28 06:32:36 -0800

    GEN-276 Create a Postgres DB Server grafana dashboard
    https://doolin.atlassian.net/browse/GEN-294
    
    Extract the pg locks into their own dashboard for now.
    Later we might want to mix and match panels for doing
    forensics or RCA, but this is easier for getting all
    the panels defined correctly.

--- scripts/grafana/dashboard/create.sh
+++ scripts/grafana/dashboard/create.sh
@@ -4,21 +4,31 @@
 # Grafana API details
 GRAFANA_HOST="http://localhost:3000"
 GRAFANA_API_TOKEN=$GRAFANA_API_TOKEN
+API_URL="$GRAFANA_HOST/api/dashboards/db"
+
 
 # Dashboard JSON file
 # DASHBOARD_JSON="./scripts/grafana/json/dashboard.json"
 # DASHBOARD_JSON="./scripts/grafana/json/flux_query.json"
-DASHBOARD_JSON="./scripts/grafana/json/four_panel.json"
+# DASHBOARD_JSON="./scripts/grafana/json/four_panel.json"
+DASHBOARD_JSON="./scripts/grafana/json/postgres.json"
+
+# DASHBOARD_JSON=(
+#     "./scripts/grafana/json/dashboard.json"
+#     "./scripts/grafana/json/flux_query.json"
+#     "./scripts/grafana/json/four_panel.json"
+# )
+
+# This Bash script snippet defines DASHBOARD_JSON as an array
+# containing three file paths. You can access the elements of
+# this array using their index (starting from 0) like this:
+# DASHBOARD_JSON[0], DASHBOARD_JSON[1], and so on.
 
 # Check if the dashboard JSON file exists
 if [ ! -f "$DASHBOARD_JSON" ]; then
     echo "Dashboard JSON file not found: $DASHBOARD_JSON"
     exit 1
 fi
-
-
-# API URL for dashboard creation
-API_URL="$GRAFANA_HOST/api/dashboards/db"
 
 # Make the API request to create the dashboard
 curl -X POST "$API_URL" \
