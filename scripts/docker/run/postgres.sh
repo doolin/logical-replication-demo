240d898d5949d0b8b5b9fa740744d31013d1217c
dave doolin <david.doolin@gmail.com>
2023-12-29 07:58:22 -0800

    GEN-302 Shellcheck the logical replication docker scripts
    https://doolin.atlassian.net/browse/GEN-302
    
    First of several commits for shellcheck. These are
    being done a few scripts at a time as they need to
    be manually verified. Later shellcheck will be hooked
    up as a github action.

--- scripts/docker/run/postgres.sh
+++ scripts/docker/run/postgres.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 IMAGE_NAME="pubsub"
-DOCKERFILE_PATH="."
-CONFIG_FILE_PATH="."
+# DOCKERFILE_PATH="."
+# CONFIG_FILE_PATH="."
 CONTAINERS=("subscriber1" "subscriber2" "publisher")
 
 for CONTAINER_NAME in "${CONTAINERS[@]}"; do
-  if docker ps -a | grep -qw $CONTAINER_NAME; then
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME # not needed when container is removed with --rm
+  if docker ps -a | grep -qw "$CONTAINER_NAME"; then
+    docker stop "$CONTAINER_NAME"
+    docker rm "$CONTAINER_NAME" # not needed when container is removed with --rm
   fi
 done
 
@@ -20,7 +20,7 @@ fi
 
 # PostgresQL databases
 docker buildx build . -t $IMAGE_NAME # -f $DOCKERFILE_PATH .
-docker run -d --name subscriber1 -m $MEMORY --memory-swap $MEMORY -p 5433:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
-docker run -d --name subscriber2 -m $MEMORY --memory-swap $MEMORY -p 5434:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
-docker run -d --name publisher   -m $MEMORY --memory-swap $MEMORY -p 5435:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
+docker run -d --name subscriber1 -m "$MEMORY" --memory-swap "$MEMORY" -p 5433:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
+docker run -d --name subscriber2 -m "$MEMORY" --memory-swap "$MEMORY" -p 5434:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
+docker run -d --name publisher   -m "$MEMORY" --memory-swap "$MEMORY" -p 5435:5432 -e POSTGRES_PASSWORD=foobar $IMAGE_NAME
 
