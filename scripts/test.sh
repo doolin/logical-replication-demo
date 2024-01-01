afd104e831038b91b04e0f8334f0b2d39ac56310
Dave Doolin <david.doolin@gmail.com>
2024-01-01 05:15:29 -0800

    GEN-303 Shellcheck update on test.sh (#53)
    https://doolin.atlassian.net/browse/GEN-303
    
    Shellcheck runs clean on test.sh, which was also
    moved to the scripts directory to help clean up
    the repository structure.

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,10 +8,10 @@ DB_HOST="localhost"
 DB_PORT="5433"
 
 # This doesn't work, fix it later.
-COUNT_QUERY="SELECT COUNT(*) FROM books;"
+# COUNT_QUERY="SELECT COUNT(*) FROM books;"
 
 ROW_COUNT=$(PGPASSWORD=$DB_PASS psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -t -c "SELECT COUNT(*) FROM books;")
-ROW_COUNT=$(echo $ROW_COUNT | xargs)
+ROW_COUNT=$(echo "$ROW_COUNT" | xargs)
 
 if [ "$ROW_COUNT" -eq 2 ]; then
     echo "Subscriber 1 Test passed!"
@@ -23,7 +23,7 @@ fi
 DB_PORT="5434"
 # Run the query to count rows in the books table
 ROW_COUNT=$(PGPASSWORD=$DB_PASS psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -t -c "SELECT COUNT(*) FROM books;")
-ROW_COUNT=$(echo $ROW_COUNT | xargs)
+ROW_COUNT=$(echo "$ROW_COUNT" | xargs)
 
 if [ "$ROW_COUNT" -eq 3 ]; then
     echo "Subscriber 2 Test passed!"
@@ -37,7 +37,7 @@ DB_NAME="publisher"
 # Run the query to count rows in the books table
 ROW_COUNT=$(PGPASSWORD=$DB_PASS psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -t -c "SELECT COUNT(*) FROM books;")
 # Remove leading/trailing white space from ROW_COUNT
-ROW_COUNT=$(echo $ROW_COUNT | xargs)
+ROW_COUNT=$(echo "$ROW_COUNT" | xargs)
 
 if [ "$ROW_COUNT" -eq 5 ]; then
     echo "Publisher Test passed!"
