db163e9ae545670a662c6dfc9c8832a14d71aa7e
dave doolin <david.doolin@gmail.com>
2024-01-12 14:56:39 -0800

    GEN-353 Fix pg_bench specs
    https://doolin.atlassian.net/browse/GEN-353
    
    A ChatGPT session with a bit of conversation
    cleaned these specs right up.

--- spec/pg_bench_spec.rb
+++ spec/pg_bench_spec.rb
@@ -3,25 +3,62 @@
 require_relative '../lib/pg_bench'
 
 RSpec.describe PGBench do
-  describe '#default' do
-    it 'returns the default pgbench command' do
-      expect(described_class.new.default)
-        .to eq('PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -s 10 -T 60 -c 10 -j 3 publisher')
+  subject(:pgbench) { described_class.new(default_options) }
+
+  let(:default_options) { {} }
+
+  describe '#initialize' do
+    context 'without options' do
+      it 'initializes with default options' do
+        expect(pgbench.options).to eq(default_options)
+      end
+
+      # Add more tests for default initializations...
+    end
+
+    context 'with custom options' do
+      subject(:pgbench_with_options) { described_class.new(custom_options) }
+
+      let(:custom_options) { { duration: 60, clients: 5 } }
+
+      it 'initializes with custom options' do
+        expect(pgbench_with_options.options).to eq(custom_options)
+      end
+
+      # Add more tests for custom initializations...
     end
   end
 
-  describe '#run_default' do
-    let(:pgbench) { described_class.new }
+  describe '#pgbench' do
+    it 'returns a valid pgbench command' do
+      expected = <<~CMD
+        PGPASSWORD=foobar pgbench \\
+          -h localhost \\
+          -p 5435 \\
+          -U postgres \\
+          -s 10 \\
+          -T 120 \\
+          -c 10 \\
+          -j 3 \\
+          publisher --log
+      CMD
 
-    before do
-      allow(pgbench).to receive(:bench_sys)
+      expect(pgbench.pgbench).to eq(expected)
     end
 
-    it 'calls bench_sys with default command' do
-      default_command = 'PGPASSWORD=foobar pgbench -h localhost -p 5435 -U postgres -s 10 -T 60 -c 10 -j 3 publisher'
-      pgbench.run_default
+    # Add more tests for variations in pgbench command...
+  end
 
-      expect(pgbench).to have_received(:bench_sys).with(default_command)
+  # Additional tests for #pgbench_custom, #pulse, #custom, etc.
+
+  describe '#bench_sys' do
+    let(:command) { 'echo "Hello"' }
+
+    it 'executes a system command' do
+      expect(pgbench).to receive(:system).with(command)
+      pgbench.bench_sys(command)
     end
+
+    # More tests for system command execution...
   end
 end
