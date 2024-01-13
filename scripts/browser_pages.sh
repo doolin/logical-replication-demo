0f816d2c473ef1d150d2e2abb747ecaf71faea2b
dave doolin <david.doolin@gmail.com>
2024-01-13 05:25:18 -0800

    GEN-317 Add script for opening all the browser pages
    https://doolin.atlassian.net/browse/GEN-317
    
    There are a lot of browser pages associated with
    running an application. This script automates opening
    all of them at once. Best to run it with a browser
    window containing very few tabs.

---
+++ scripts/browser_pages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# infotext "opening logstash, elastic search, etc"
+
+# Elasticsearch
+open http://localhost:9201/
+
+# Kibana
+open http://localhost:5601/app/home#/
+
+# Rails, broken
+# http://localhost:3002/
+
+# PGHero
+open http://localhost:8080/
+
+# InfluxDB
+open http://localhost:8086/orgs
+
+# Grafana
+open http://localhost:3000/dashboards
+
+# Sentry
+open https://dave-doolin.sentry.io/issues/
+
+# Stathat
+open https://www.stathat.com/
