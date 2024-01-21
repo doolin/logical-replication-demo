7e35a195ebdfdbf9fb185b82e72d53d831329b68
Dave Doolin <david.doolin@gmail.com>
2024-01-21 05:03:26 -0800

    GEN-211 Break the publisher db or container (#105)
    https://doolin.atlassian.net/browse/GEN-211
    
    This was a 2 hour project which turned into
    over a day of tracked time and 7 child tickets
    shipped in support. In the end, not a lot was
    directly learned or accomplished, but the
    tooling will be very helpful in the near future.

--- experiments/cyclic_load.sh
+++ experiments/cyclic_load.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# The  intent of this script is to invoke a Ruby script that
+# The intent of this script is to invoke a Ruby script that
 # produces a cyclic load on the database. The script will be
 # able to execute in the background, by itself, or in combination
 # with other scripts.
-
-# Ensure the database is initialized for pgbench.
-# source pgbench.sh -T 1
-# sleep 1
 
 DURATION=300
 nohup ./exe/background_hum.rb -T $DURATION &
